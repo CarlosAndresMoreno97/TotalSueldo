c616137b18bcaabbbfc6033696a0e65f3eae6435
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2023-09-01 11:59:50 -0500

    Sueldo total

---
+++ TotalSueldo/Program.cs
@@ -0,0 +1,30 @@
+﻿//Enuciado:Un vendedor recibe un sueldo base más un 10% extra por comisión de sus ventas,
+//el vendedor desea saber cuánto dinero obtendrá por concepto de comisiones
+//por las tres ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta
+//su sueldo base y comisiones. 
+
+
+public class TotalSueldo
+{
+    static void Main(string[] args)
+    {
+        double sueldobase, valorventa, comision, sueldototal;
+
+        Console.Write("Ingresar su sueldo base: ");
+        sueldobase = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresar valor de venta: ");
+        valorventa = Convert.ToDouble(Console.ReadLine());
+
+        comision = (valorventa * 10) / 100;
+        sueldototal = sueldobase + comision;
+
+        Console.Write("La comisio es: " + comision);
+        Console.Write("\n");
+        Console.Write("El sueldo total mensual es: " +  sueldototal);
+        Console.Write("\n");
+
+    }
+
+}
+
